59202b85f05aa13a04a004e29cd17627f7876946
radlinskii <radlinskiignacy@gmail.com>
2023-07-25 20:10:10 +0200

    refactor: rename neovim folder to fix name clash with CDPATH

--- scripts/install.sh
+++ scripts/install.sh
@@ -22,7 +22,7 @@ create_symlink "wezterm/wezterm.lua" "$HOME/.config/wezterm";
 
 create_symlink "less/.lesskey" "$HOME";
 
-create_symlink "nvim/init.lua" "$HOME/.config/nvim";
+create_symlink "neovim/init.lua" "$HOME/.config/nvim";
 
 create_symlink "zsh/.custom_aliases" "$HOME";
 create_symlink "zsh/.custom_bindings" "$HOME";
