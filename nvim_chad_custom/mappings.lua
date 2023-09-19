717bf8db87dfc4e09718fbf78d1267a2a8ba5c84
radlinskii <radlinskiignacy@gmail.com>
2023-09-19 11:45:10 +0200

    nvim: remove duplications from mappings

--- nvim_chad_custom/mappings.lua
+++ nvim_chad_custom/mappings.lua
@@ -1,82 +1,57 @@
 ---@type MappingsTable
 local M = {}
 
+local noremapOpts = { noremap = true }
+local noremapExprOpts = { noremap = true, expr = true }
+local noremapNowaitOpts = { noremap = true, nowait = true }
+
+local colemakMappings = {
+    ["<Up>"] = { 'v:count || mode(1)[0:1] == "no" ? "k" : "gk"', "Move up", opts = noremapExprOpts },
+    ["<Down>"] = { 'v:count || mode(1)[0:1] == "no" ? "j" : "gj"', "Move down", opts = noremapExprOpts },
+
+    ["u"] = { 'v:count || mode(1)[0:1] == "no" ? "k" : "gk"', "Move up", opts = noremapExprOpts },
+    ["e"] = { 'v:count || mode(1)[0:1] == "no" ? "j" : "gj"', "Move down", opts = noremapExprOpts },
+    ["n"] = { "h", "Move left", opts = noremapOpts },
+    ["i"] = { "l", "Move right", opts = noremapOpts },
+
+    ["N"] = { "H", "move to beginning of the visible screen", opts = noremapOpts },
+    ["I"] = { "L", "move to beginning of the visible screen", opts = noremapOpts },
+    ["h"] = { "i", "insert mode in place", opts = noremapOpts },
+    ["H"] = { "I", "start insert mode at the non-blank spaced beginning of current line", opts = noremapOpts },
+    ["k"] = { "u", "undo last change", opts = noremapOpts },
+    ["K"] = { "U", "undo all changes", opts = noremapOpts },
+    ["j"] = { "nzzzv", "find next appearance", opts = noremapOpts },
+    ["J"] = { "Nzzzv", "find previous appearance", opts = noremapOpts },
+    ["l"] = { "e", "go to end of the next word", opts = noremapOpts },
+    ["L"] = { "E", "go to the end of the next non-blank spaced word", opts = noremapOpts },
+    ["gl"] = { "ge", "go to end of the previous word", opts = noremapOpts },
+    ["gL"] = { "gE", "go to end of the previous non-blank spaced word", opts = noremapOpts },
+    ["E"] = { "J", "remove line break", opts = noremapOpts },
+
+    ["<C-l>"] = { "<C-d>zz", "scroll half a window down and move cursor to the middle", opts = noremapOpts },
+    ["<C-y>"] = { "<C-u>zz", "scroll half a window up and move cursor to the middle", opts = noremapOpts },
+    ["<PageUp>"] = { "<PageUp>zz", "scroll a whole window up and move cursor to the middle", opts = noremapOpts },
+    ["<PageDown>"] = {
+        "<PageDown>zz",
+        "scroll a whole window down and move cursor to the middle",
+        opts = noremapOpts,
+    },
+}
+
 M.general = {
     i = {},
-    n = {
-        -- switch between windows
+    n = vim.tbl_deep_extend("force", colemakMappings, {
+        [";"] = { ":", "enter command mode", opts = noremapNowaitOpts },
+
         ["<C-u>"] = { "<C-w>k", "Window up" },
         ["<C-e>"] = { "<C-w>j", "Window down" },
         ["<C-n>"] = { "<C-w>h", "Window left" },
         ["<C-i>"] = { "<C-w>l", "Window right" },
 
-        [";"] = { ":", "enter command mode", opts = { nowait = true } },
-
-        ["u"] = {
-            'v:count || mode(1)[0:1] == "no" ? "k" : "gk"',
-            "Move up",
-            opts = { expr = true, noremap = true, silent = true },
-        },
-        ["e"] = {
-            'v:count || mode(1)[0:1] == "no" ? "j" : "gj"',
-            "Move down",
-            opts = { expr = true, noremap = true, silent = true },
-        },
-        ["<Up>"] = {
-            'v:count || mode(1)[0:1] == "no" ? "k" : "gk"',
-            "Move up",
-            opts = { expr = true, noremap = true, silent = true },
-        },
-        ["<Down>"] = {
-            'v:count || mode(1)[0:1] == "no" ? "j" : "gj"',
-            "Move down",
-            opts = { expr = true, noremap = true, silent = true },
-        },
-        ["n"] = { "h", "Move left", opts = { noremap = true, silent = true } },
-        ["i"] = { "l", "Move right", opts = { noremap = true, silent = true } },
-        ["N"] = { "H", "move to beginning of the visible screen", opts = { noremap = true, silent = true } },
-        ["I"] = { "L", "move to beginning of the visible screen", opts = { noremap = true, silent = true } },
-        ["h"] = { "i", "insert mode in place", opts = { noremap = true, silent = true } },
-        ["H"] = {
-            "I",
-            "start insert mode at the non-blank spaced beginning of current line",
-            opts = { noremap = true, silent = true },
-        },
-        ["k"] = { "u", "undo last change", opts = { noremap = true, silent = true } },
-        ["K"] = { "U", "undo all changes", opts = { noremap = true, silent = true } },
-        ["j"] = { "nzzzv", "find next appearance", opts = { noremap = true, silent = true } },
-        ["J"] = { "Nzzzv", "find previous appearance", opts = { noremap = true, silent = true } },
-        ["l"] = { "e", "go to end of the next word", opts = { noremap = true, silent = true } },
-        ["L"] = { "E", "go to the end of the next non-blank spaced word", opts = { noremap = true, silent = true } },
-        ["gl"] = { "ge", "go to end of the previous word", opts = { noremap = true, silent = true } },
-        ["gL"] = { "gE", "go to end of the previous non-blank spaced word", opts = { noremap = true, silent = true } },
-        ["E"] = { "J", "remove line break", opts = { noremap = true, silent = true } },
-
-        ["<C-l>"] = {
-            "<C-d>zz",
-            "scroll half a window down and move cursor to the middle",
-            opts = { noremap = true, silent = true },
-        },
-        ["<C-y>"] = {
-            "<C-u>zz",
-            "scroll half a window up and move cursor to the middle",
-            opts = { noremap = true, silent = true },
-        },
-        ["<PageUp>"] = {
-            "<PageUp>zz",
-            "scroll a whole window up and move cursor to the middle",
-            opts = { noremap = true, silent = true },
-        },
-        ["<PageDown>"] = {
-            "<PageDown>zz",
-            "scroll a whole window down and move cursor to the middle",
-            opts = { noremap = true, silent = true },
-        },
-
         ["<leader>rs"] = {
             [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
-            "replace current word",
-            opts = { noremap = true, silent = true },
+            "replace the whole current word",
+            opts = noremapOpts,
         },
 
         ["<C-d>"] = {
@@ -84,161 +59,35 @@ M.general = {
                 vim.diagnostic.goto_next({ float = { border = "rounded" } })
             end,
             "Go to next error",
-            opts = { noremap = true, silent = true },
+            opts = noremapOpts,
         },
         ["<C-S-d>"] = {
             function()
                 vim.diagnostic.goto_prev({ float = { border = "rounded" } })
             end,
             "Go to prev error",
-            opts = { noremap = true, silent = true },
+            opts = noremapOpts,
         },
-        ["<leader>fp"] = {
-            "<cmd>Telescope diagnostics<CR>",
-            "open telescope diagnostics",
-            opts = { noremap = true, silent = true },
-        },
+        ["<leader>fp"] = { "<cmd>Telescope diagnostics<CR>", "open telescope diagnostics", opts = noremapOpts },
 
         ["<leader>ff"] = {
             "<cmd>lua require'telescope.builtin'.find_files({ find_command = {'rg', '--files', '--hidden', '-g', '!.git' }})<cr>",
             "find files, including hidden files, but those in .git folder ",
-            opts = { noremap = true, silent = true },
+            opts = noremapOpts,
         },
-    },
+    }),
     v = {
         [">"] = { ">gv", "indent" },
 
-        ["<Up>"] = {
-            'v:count || mode(1)[0:1] == "no" ? "k" : "gk"',
-            "Move up",
-            opts = { expr = true, noremap = true },
-        },
-        ["<Down>"] = {
-            'v:count || mode(1)[0:1] == "no" ? "j" : "gj"',
-            "Move down",
-            opts = { expr = true, noremap = true },
-        },
-        ["E"] = {
-            ":m '>+1<CR>gv=gv",
-            "move line up",
-            opts = { noremap = true },
-        },
-        ["U"] = {
-            ":m '<-2<CR>gv=gv",
-            "move line down",
-            opts = { noremap = true },
-        },
-
-        ["<C-l>"] = {
-            "<C-d>zz",
-            "scroll half a window down and move cursor to the middle",
-            opts = { noremap = true, silent = true },
-        },
-        ["<C-y>"] = {
-            "<C-u>zz",
-            "scroll half a window up and move cursor to the middle",
-            opts = { noremap = true, silent = true },
-        },
-        ["<PageUp>"] = {
-            "<PageUp>zz",
-            "scroll a whole window up and move cursor to the middle",
-            opts = { noremap = true, silent = true },
-        },
-        ["<PageDown>"] = {
-            "<PageDown>zz",
-            "scroll a whole window down and move cursor to the middle",
-            opts = { noremap = true, silent = true },
-        },
+        ["E"] = { ":m '>+1<CR>gv=gv", "move selected lines up", opts = noremapOpts },
+        ["U"] = { ":m '<-2<CR>gv=gv", "move selected lines down", opts = noremapOpts },
     },
-    x = {
-        ["u"] = {
-            'v:count || mode(1)[0:1] == "no" ? "k" : "gk"',
-            "Move up",
-            opts = { expr = true, noremap = true },
-        },
-        ["e"] = {
-            'v:count || mode(1)[0:1] == "no" ? "j" : "gj"',
-            "Move down",
-            opts = { expr = true, noremap = true, silent = true },
-        },
-        ["<Up>"] = {
-            'v:count || mode(1)[0:1] == "no" ? "k" : "gk"',
-            "Move up",
-            opts = { expr = true, noremap = true },
-        },
-        ["<Down>"] = {
-            'v:count || mode(1)[0:1] == "no" ? "j" : "gj"',
-            "Move down",
-            opts = { expr = true, noremap = true },
-        },
-        ["n"] = { "h", "Move left", opts = { noremap = true } },
-        ["i"] = { "l", "Move right", opts = { noremap = true } },
-
-        ["N"] = { "H", "move to beginning of the visible screen", opts = { noremap = true } },
-        ["I"] = { "L", "move to beginning of the visible screen", opts = { noremap = true } },
-        ["h"] = { "i", "insert mode in place", opts = { noremap = true } },
-        ["H"] = {
-            "I",
-            "start insert mode at the non-blank spaced beginning of current line",
-            opts = { noremap = true },
-        },
-        ["k"] = { "u", "undo last change", opts = { noremap = true } },
-        ["K"] = { "U", "undo all changes", opts = { noremap = true } },
-        ["j"] = { "n", "find next appearance", opts = { noremap = true } },
-        ["J"] = { "N", "find previous appearance", opts = { noremap = true } },
-        ["l"] = { "e", "go to end of the next word", opts = { noremap = true } },
-        ["L"] = { "E", "go to the end of the next non-blank spaced word", opts = { noremap = true } },
-        ["gl"] = { "ge", "go to end of the previous word", opts = { noremap = true } },
-        ["gL"] = { "gE", "go to end of the previous non-blank spaced word", opts = { noremap = true } },
-        ["E"] = { "J", "remove line break", opts = { noremap = true } },
-    },
-    o = {
-        ["u"] = {
-            'v:count || mode(1)[0:1] == "no" ? "k" : "gk"',
-            "Move up",
-            opts = { expr = true, noremap = true },
-        },
-        ["e"] = {
-            'v:count || mode(1)[0:1] == "no" ? "j" : "gj"',
-            "Move down",
-            opts = { expr = true, noremap = true, silent = true },
-        },
-        ["<Up>"] = {
-            'v:count || mode(1)[0:1] == "no" ? "k" : "gk"',
-            "Move up",
-            opts = { expr = true, noremap = true },
-        },
-        ["<Down>"] = {
-            'v:count || mode(1)[0:1] == "no" ? "j" : "gj"',
-            "Move down",
-            opts = { expr = true, noremap = true },
-        },
-        ["n"] = { "h", "Move left", opts = { noremap = true } },
-        ["i"] = { "l", "Move right", opts = { noremap = true } },
-
-        ["N"] = { "H", "move to beginning of the visible screen", opts = { noremap = true } },
-        ["I"] = { "L", "move to beginning of the visible screen", opts = { noremap = true } },
-        ["h"] = { "i", "insert mode in place", opts = { noremap = true } },
-        ["H"] = {
-            "I",
-            "start insert mode at the non-blank spaced beginning of current line",
-            opts = { noremap = true },
-        },
-        ["k"] = { "u", "undo last change", opts = { noremap = true } },
-        ["K"] = { "U", "undo all changes", opts = { noremap = true } },
-        ["j"] = { "n", "find next appearance", opts = { noremap = true } },
-        ["J"] = { "N", "find previous appearance", opts = { noremap = true } },
-        ["l"] = { "e", "go to end of the next word", opts = { noremap = true } },
-        ["L"] = { "E", "go to the end of the next non-blank spaced word", opts = { noremap = true } },
-        ["gl"] = { "ge", "go to end of the previous word", opts = { noremap = true } },
-        ["gL"] = { "gE", "go to end of the previous non-blank spaced word", opts = { noremap = true } },
-        ["E"] = { "J", "remove line break", opts = { noremap = true } },
-    },
+    x = vim.tbl_deep_extend("force", colemakMappings, {}),
+    o = vim.tbl_deep_extend("force", colemakMappings, {}),
 }
 
 M.nvimtree = {
     n = {
-        -- toggle
         ["<C-b>"] = { "<cmd> NvimTreeToggle <CR>", "Toggle nvimtree" },
     },
 }
