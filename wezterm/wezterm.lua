1ff578eb59957901809b9f9d22d80555443b53ca
radlinskii <radlinskiignacy@gmail.com>
2023-09-20 15:30:19 +0200

    wezterm: disable quickselectmode keymap

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,5 +1,5 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This table will hold the configuration.
 local config = {}
@@ -16,14 +16,14 @@ local function get_appearance()
     if wezterm.gui then
         return wezterm.gui.get_appearance()
     end
-    return 'Dark'
+    return "Dark"
 end
 
 local function scheme_for_appearance(appearance)
-    if appearance:find 'Dark' then
-        return 'SpaceGray Eighties Dull'
+    if appearance:find("Dark") then
+        return "SpaceGray Eighties Dull"
     else
-        return 'Ivory Light (terminal.sexy)'
+        return "Ivory Light (terminal.sexy)"
     end
 end
 
@@ -35,8 +35,8 @@ config.color_scheme = scheme_for_appearance(get_appearance())
 local function common_tab_bar_colors()
     return {
         active_tab = {
-            bg_color = '#2b2042',
-            fg_color = '#efe0ef',
+            bg_color = "#2b2042",
+            fg_color = "#efe0ef",
             italic = true,
             strikethrough = false,
         },
@@ -44,20 +44,20 @@ local function common_tab_bar_colors()
 end
 
 local function colors_for_appearance(appearance)
-    if appearance:find 'Dark' then
+    if appearance:find("Dark") then
         return {
             -- The default text color
-            foreground = '#d0d0d0',
-            background = '#1b1b00',
-            selection_fg = 'black',
-            selection_bg = '#fffacd',
+            foreground = "#d0d0d0",
+            background = "#1b1b00",
+            selection_fg = "black",
+            selection_bg = "#fffacd",
             tab_bar = common_tab_bar_colors(),
         }
     else
         return {
             -- The default text color
-            foreground = '#303030',
-            background = '#f8f1df',
+            foreground = "#303030",
+            background = "#f8f1df",
             tab_bar = common_tab_bar_colors(),
         }
     end
@@ -66,50 +66,55 @@ end
 config.colors = colors_for_appearance(get_appearance())
 
 config.window_frame = {
-    font = wezterm.font { family = 'Roboto', weight = 'Bold' },
+    font = wezterm.font({ family = "Roboto", weight = "Bold" }),
     font_size = 14.0,
-    active_titlebar_bg = '#22aa3f', -- not working as expected
-    inactive_titlebar_bg = '#44996f',
+    active_titlebar_bg = "#22aa3f", -- not working as expected
+    inactive_titlebar_bg = "#44996f",
 }
 
-config.font = wezterm.font 'JetBrains Mono'
+config.font = wezterm.font("JetBrains Mono")
 config.font_size = 13.6
 
-config.leader = { key = 'e', mods = 'CMD', timeout_milliseconds = 1000 }
+config.leader = { key = "e", mods = "CMD", timeout_milliseconds = 1000 }
 config.keys = {
     -- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
-    { key = "LeftArrow",  mods = "OPT", action = wezterm.action { SendString = "\x1bb" } },
+    { key = "LeftArrow", mods = "OPT", action = wezterm.action({ SendString = "\x1bb" }) },
     -- Make Option-Right equivalent to Alt-f; forward-word
-    { key = "RightArrow", mods = "OPT", action = wezterm.action { SendString = "\x1bf" } },
-    { key = "Backspace",  mods = "CMD", action = wezterm.action { SendString = "\x15" } },
+    { key = "RightArrow", mods = "OPT", action = wezterm.action({ SendString = "\x1bf" }) },
+    { key = "Backspace", mods = "CMD", action = wezterm.action({ SendString = "\x15" }) },
     {
-        key = 'i',
-        mods = 'CMD',
-        action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+        key = "i",
+        mods = "CMD",
+        action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
     },
     {
-        key = '.',
-        mods = 'CMD',
-        action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+        key = ".",
+        mods = "CMD",
+        action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
     },
     {
-        key = 'c',
-        mods = 'CMD|SHIFT',
+        key = "c",
+        mods = "CMD|SHIFT",
         action = wezterm.action.ActivateCopyMode,
     },
     {
-        key = 'm',
-        mods = 'CMD',
+        key = "m",
+        mods = "CMD",
         action = wezterm.action.DisableDefaultAssignment,
     },
     {
-        key = 'h',
-        mods = 'CMD',
+        key = "h",
+        mods = "CMD",
+        action = wezterm.action.DisableDefaultAssignment,
+    },
+    {
+        key = "Space",
+        mods = "CTRL|SHIFT",
         action = wezterm.action.DisableDefaultAssignment,
     },
 }
 
-wezterm.on('gui-startup', function(cmd)
+wezterm.on("gui-startup", function(cmd)
     local tab, pane, window = wezterm.mux.spawn_window(cmd or {})
     window:gui_window():maximize()
 end)
