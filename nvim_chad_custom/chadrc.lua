4b23fb99c777a4752f24aaef758a48ee766ec2f7
radlinskii <radlinskiignacy@gmail.com>
2023-09-22 16:55:28 +0200

    nvim: add nvdash at startup screen

--- nvim_chad_custom/chadrc.lua
+++ nvim_chad_custom/chadrc.lua
@@ -3,6 +3,7 @@ local M = {}
 
 -- Path to overriding theme and highlights files
 local highlights = require("custom.highlights")
+local NVIM_VERSION = vim.fn.system("nvim --version | head -n 1 | awk '{print $2}'"):gsub("\n", "")
 
 M.ui = {
     theme = "penumbra_light",
@@ -43,6 +44,37 @@ M.ui = {
     },
     telescope = {
         style = "bordered",
+    },
+
+    nvdash = {
+        load_on_startup = true,
+
+        header = {
+            [[     ___           ___           ___           ___                       ___      ]],
+            [[    /\__\         /\  \         /\  \         /\__\          ___        /\__\     ]],
+            [[   /::|  |       /::\  \       /::\  \       /:/  /         /\  \      /::|  |    ]],
+            [[  /:|:|  |      /:/\:\  \     /:/\:\  \     /:/  /          \:\  \    /:|:|  |    ]],
+            [[ /:/|:|  |__   /::\~\:\  \   /:/  \:\  \   /:/__/  ___      /::\__\  /:/|:|__|__  ]],
+            [[/:/ |:| /\__\ /:/\:\ \:\__\ /:/__/ \:\__\  |:|  | /\__\  __/:/\/__/ /:/ |::::\__\ ]],
+            [[\/__|:|/:/  / \:\~\:\ \/__/ \:\  \ /:/  /  |:|  |/:/  / /\/:/  /    \/__/~~/:/  / ]],
+            [[    |:/:/  /   \:\ \:\__\    \:\  /:/  /   |:|__/:/  /  \::/__/           /:/  /  ]],
+            [[    |::/  /     \:\ \/__/     \:\/:/  /     \::::/__/    \:\__\          /:/  /   ]],
+            [[    /:/  /       \:\__\        \::/  /        ‾‾‾         \/__/         /:/  /    ]],
+            [[    \/__/         \/__/         \/__/                                   \/__/     ]],
+            [[                                                                                  ]],
+            [[                                                                           ]] .. NVIM_VERSION,
+        },
+
+        buttons = {
+            { "󰈚  Recent Files", "<Space> f o", "Telescope oldfiles" },
+            { "  Find File", "<Space> f f", "Telescope find_files" },
+            { "󰈭  Find Word", "<Space> f w", "Telescope live_grep" },
+            { "  Git Status", "<Space> f g s", "Telescope git_status" },
+            { "  Bookmarks", "<Space> f a", "Telescope marks" },
+            { "  Themes", "<Space> , f", "Telescope themes" },
+            { "  Mappings", "<Space> , m", "NvCheatsheet" },
+            { "󰮥  Help", "<Space> f h", "Telescope help_tags" },
+        },
     },
 }
 
