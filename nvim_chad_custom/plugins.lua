4e1601050632658e25df13225a7fe71b9618402c
radlinskii <radlinskiignacy@gmail.com>
2023-09-25 17:11:17 +0200

    nvim(undotree): extract undotree config

--- nvim_chad_custom/plugins.lua
+++ nvim_chad_custom/plugins.lua
@@ -69,21 +69,7 @@ local plugins = {
         "mbbill/undotree",
         event = "BufReadPre",
         config = function()
-            vim.g.undotree_SetFocusWhenToggle = 1
-            vim.g.undotree_DiffAutoOpen = 0
-            vim.g.undotree_WindowLayout = 2
-            vim.g.undotree_HelpLine = 0
-            vim.g.Undotree_CustomMap = function()
-                local b = { buffer = 0 }
-                vim.keymap.set("n", "q", "<Plug>UndotreeClose", b)
-                vim.keymap.set("n", "u", "<plug>UndotreeNextState", b)
-                vim.keymap.set("n", "e", "<plug>UndotreePreviousState", b)
-                vim.keymap.set("n", "k", "<plug>UndotreeUndo", b)
-                vim.keymap.set("n", "J", "<Nop>", b)
-                vim.keymap.set("n", "K", "<Nop>", b)
-                vim.keymap.set("n", "U", "<Nop>", b)
-                vim.keymap.set("n", "C", "<Nop>", b)
-            end
+            require("custom.configs.undotree")
         end,
     },
 
