f482a24212199fc11093824e8c65f41524a004c0
radlinskii <radlinskiignacy@gmail.com>
2023-09-26 12:48:28 +0200

    nvim(ctrlsf): adjust mapping

--- nvim_chad_custom/configs/ctrlsf.lua
+++ nvim_chad_custom/configs/ctrlsf.lua
@@ -1,19 +1,26 @@
 vim.g.ctrlsf_mapping = {
     open = { "<CR>", "o" },
     openb = "O",
-    split = "<C-O>",
-    vsplit = "",
+    split = "<C-H>",
+    vsplit = "<C-V>",
     tab = "t",
     tabb = "T",
     popen = "p",
     popenf = "P",
-    quit = "Q",
-    next = "<C-J>",
-    prev = "<C-K>",
-    nfile = "<C-N>",
-    pfile = "<C-P>",
+    quit = "q",
+    next = ")",
+    prev = "(",
+    nfile = "]",
+    pfile = "[",
     pquit = "q",
     loclist = "",
     chgmode = "M",
     stop = "<C-C>",
 }
+
+vim.g.ctrlsf_auto_focus = {
+    at = "done",
+    duration_less_than = 1000,
+}
+
+vim.g.ctrlsf_winsize = 80
