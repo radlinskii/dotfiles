d0f4117151bcf69ea6fb957c31e20f8ccd84b454
radlinskii <radlinskiignacy@gmail.com>
2023-10-04 15:46:24 +0200

    nvim: moved undodir to local folder

--- nvim_chad_custom/configs/undotree.lua
+++ nvim_chad_custom/configs/undotree.lua
@@ -1,5 +1,5 @@
 if vim.fn.has("persistent_undo") == 1 then
-    local target_path = vim.fn.expand("~/.config/nvim/.undo")
+    local target_path = vim.fn.expand("~/.local/share/nvim/custom_undodir")
 
     if vim.fn.isdirectory(target_path) == false then
         os.execute("mkdir -p " .. target_path)
