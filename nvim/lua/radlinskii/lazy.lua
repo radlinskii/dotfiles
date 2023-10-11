d127f04f33ec2c3e8429685244e4924e6c50a14c
radlinskii <radlinskiignacy@gmail.com>
2023-10-11 17:28:54 +0200

    add noice trouble and todo-comments

--- nvim/lua/radlinskii/lazy.lua
+++ nvim/lua/radlinskii/lazy.lua
@@ -14,7 +14,7 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({ { import = "radlinskii.plugins" } }, {
     install = {
-        colorscheme = { "tokyonight" },
+        colorscheme = { "kanagawa" },
     },
     checker = {
         enabled = true,
