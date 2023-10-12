bfed207f7756e885e7bcf2afebc949a66b4d7a8c
radlinskii <radlinskiignacy@gmail.com>
2023-10-12 15:19:37 +0200

    add auto-session

--- nvim/lua/radlinskii/plugins/lualine.lua
+++ nvim/lua/radlinskii/plugins/lualine.lua
@@ -1,16 +1,18 @@
 return {
     "nvim-lualine/lualine.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
+    dependencies = { "nvim-tree/nvim-web-devicons", "rmagatti/auto-session", "rebelot/kanagawa.nvim" },
     event = { "VimEnter" },
     config = function()
         local lualine = require("lualine")
         local lazy_status = require("lazy.status") -- to configure lazy pending updates count
+        local current_session_name = require("auto-session.lib").current_session_name
 
         lualine.setup({
             options = {
                 theme = require("lualine.themes.kanagawa"),
             },
             sections = {
+                lualine_b = { current_session_name, "branch", "diff", "diagnostics" },
                 lualine_x = {
                     {
                         lazy_status.updates,
