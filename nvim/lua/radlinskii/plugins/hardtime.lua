03dd50d0fb8e07e5bdeb5142e3cb2cbd41d5b55b
radlinskii <radlinskiignacy@gmail.com>
2023-10-16 11:36:51 +0200

    do not block j and k with hardtime

--- nvim/lua/radlinskii/plugins/hardtime.lua
+++ nvim/lua/radlinskii/plugins/hardtime.lua
@@ -3,6 +3,10 @@ return {
     dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
     event = { "BufReadPre" },
     opts = {
+        resetting_keys = {
+            ["j"] = { "n", "x" },
+            ["k"] = { "n", "x" },
+        },
         disabled_keys = {
             ["<Up>"] = { "" },
             ["<Down>"] = { "" },
