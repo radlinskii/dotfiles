6ab2dd8d158b74ac4659381583aa37f466e29c99
radlinskii <radlinskiignacy@gmail.com>
2023-10-24 15:43:42 +0200

    update dimmed inactive windows colors

--- nvim/lua/radlinskii/plugins/colorscheme.lua
+++ nvim/lua/radlinskii/plugins/colorscheme.lua
@@ -18,13 +18,13 @@ return {
                         peachRead = "#cf3D62",
                         oniViolet = "#957Fc8",
                         -- use dragon theme background colors for wave theme
-                        sumiInk0 = "#0d0c0c",
-                        sumiInk1 = "#12120f",
-                        sumiInk2 = "#1D1C19",
-                        sumiInk3 = "#181616",
-                        sumiInk4 = "#282727",
-                        sumiInk5 = "#393836",
-                        sumiInk6 = "#625e5a",
+                        sumiInk0 = "#0d0c0c", -- dragonBlack0
+                        sumiInk1 = "#12120f", -- dragonBlack1
+                        sumiInk2 = "#1D1C19", -- dragonBlack2
+                        sumiInk3 = "#181616", -- dragonBlack3
+                        sumiInk4 = "#282727", -- dragonBlack4
+                        sumiInk5 = "#393836", -- dragonBlack5
+                        sumiInk6 = "#625e5a", -- dragonBlack6
                         -- make light theme less solarized
                         lotusWhite0 = "#e2d8b1",
                         lotusWhite1 = "#ebe6bc",
@@ -33,7 +33,20 @@ return {
                         lotusWhite4 = "#e8edcf",
                         lotusWhite5 = "#e7e8c9",
                     },
-                    theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+                    theme = {
+                        wave = {
+                            ui = {
+                                bg_dim = "#0d0c0c", -- dragonBlack0,
+                            },
+                        },
+                        lotus = {
+                            ui = {
+                                bg_dim = "#e7e8c9", -- lotusWhite5
+                            },
+                        },
+                        dragon = {},
+                        all = {},
+                    },
                 },
                 overrides = function(colors)
                     local theme = colors.theme
@@ -41,6 +54,7 @@ return {
                     return {
                         IblIndent = { fg = theme.ui.bg_gutter },
                         Substitute = { fg = theme.ui.bg, bg = theme.ui.fg },
+                        LazyNormal = { bg = theme.ui.bg, fg = theme.ui.fg },
                     }
                 end,
                 theme = "wave", -- Load "wave" theme when 'background' option is not set
