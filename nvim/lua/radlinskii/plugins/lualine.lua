8b8a67da75d4068cc3a3993e90e741a74106869a
Ignacy Radlinski <ignacy.radlinski@u-blox.com>
2023-12-12 10:35:26 +0100

    remove auto-session add auto-dark-mode

--- nvim/lua/radlinskii/plugins/lualine.lua
+++ nvim/lua/radlinskii/plugins/lualine.lua
@@ -1,18 +1,16 @@
 return {
     "nvim-lualine/lualine.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons", "rmagatti/auto-session", "rebelot/kanagawa.nvim" },
+    dependencies = { "nvim-tree/nvim-web-devicons", "rebelot/kanagawa.nvim" },
     event = { "VimEnter" },
     config = function()
         local lualine = require("lualine")
         local lazy_status = require("lazy.status") -- to configure lazy pending updates count
-        local current_session_name = require("auto-session.lib").current_session_name
 
         lualine.setup({
             options = {
-                theme = require("lualine.themes.kanagawa"),
+                theme = "auto",
             },
             sections = {
-                lualine_b = { current_session_name, "branch", "diff", "diagnostics" },
                 lualine_x = {
                     {
                         lazy_status.updates,
