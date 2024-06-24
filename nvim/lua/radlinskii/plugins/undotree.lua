340e714d93b62a03975fe5b64beb6fcf6aaa5826
radlinskii <radlinskiignacy@gmail.com>
2024-06-24 18:49:53 +0200

    fix undotree

--- nvim/lua/radlinskii/plugins/undotree.lua
+++ nvim/lua/radlinskii/plugins/undotree.lua
@@ -2,7 +2,9 @@ return {
     "mbbill/undotree",
     event = "BufReadPre",
     config = function()
-        if vim.fn.has("persistent_undo") == 1 then
+        local is_windows = require("radlinskii.utils.system").is_windows()
+
+        if vim.fn.has("persistent_undo") then
             local target_path = vim.fn.expand("~/.local/share/nvim/custom_undodir")
 
             if vim.fn.isdirectory(target_path) == false then
@@ -13,29 +15,32 @@ return {
             vim.opt.undofile = true
         end
 
+        if is_windows then
+            vim.g.undotree_DiffCommand = "FC"
+        end
         vim.g.undotree_SetFocusWhenToggle = 1
         vim.g.undotree_DiffAutoOpen = 0
         vim.g.undotree_WindowLayout = 2
         vim.g.undotree_HelpLine = 0
 
         vim.g.Undotree_CustomMap = function()
-            local b = { buffer = 0, noremap = true, silent = true }
+            local opts = { buffer = 0, noremap = true, silent = true }
 
-            vim.keymap.set("n", "q", "<Plug>UndotreeClose", b)
-            vim.keymap.set("n", "u", "<plug>UndotreeNextState", b)
-            vim.keymap.set("n", "e", "<plug>UndotreePreviousState", b)
-            vim.keymap.set("n", "k", "<plug>UndotreeUndo", b)
-            vim.keymap.set("n", "J", "<Nop>", b)
-            vim.keymap.set("n", "K", "<Nop>", b)
-            vim.keymap.set("n", "U", "<Nop>", b)
-            vim.keymap.set("n", "C", "<Nop>", b)
+            vim.keymap.set("n", "q", "<Plug>UndotreeClose", opts)
+            vim.keymap.set("n", "u", "<plug>UndotreeNextState", opts)
+            vim.keymap.set("n", "e", "<plug>UndotreePreviousState", opts)
+            vim.keymap.set("n", "k", "<plug>UndotreeUndo", opts)
+            vim.keymap.set("n", "J", "<Nop>", opts)
+            vim.keymap.set("n", "K", "<Nop>", opts)
+            vim.keymap.set("n", "U", "<Nop>", opts)
+            vim.keymap.set("n", "C", "<Nop>", opts)
         end
 
         vim.keymap.set(
             "n",
             "<leader>u",
             "<cmd> UndotreeToggle <CR>",
-            { desc = "Toggle undotree", noremap = true, silent = true }
+            { desc = "Toggle Undotree", noremap = true, silent = true }
         )
     end,
 }
