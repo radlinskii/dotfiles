968f7e0d79524d72b1f1e023a8b7e13c16f133e1
radlinskii <radlinskiignacy@gmail.com>
2024-06-26 13:23:42 +0200

    fix opening files inside running nvim instance from lazygit

--- nvim/lua/radlinskii/core/usercmds.lua
+++ nvim/lua/radlinskii/core/usercmds.lua
@@ -5,6 +5,7 @@ usercmd("ClearShada", function()
     local shada_path = vim.fn.expand(vim.fn.stdpath("data") .. "/shada")
     local files = vim.fn.glob(shada_path .. "/*", false, true)
     local all_success = 0
+
     for _, file in ipairs(files) do
         local file_name = vim.fn.fnamemodify(file, ":t")
         if file_name == "main.shada" then
@@ -22,3 +23,25 @@ usercmd("ClearShada", function()
         vim.print("Successfully deleted all temporary shada files")
     end
 end, { desc = "Clears all the .tmp shada files" })
+
+usercmd("ReopenInLastTabAtLine", function(opts)
+    local line_number = opts.args:match("^(%d+)%s+")
+    local filename = opts.args:match('%"([^%"]+)%"')
+    if line_number and filename then
+        vim.cmd("bdelete")
+        vim.cmd("edit " .. filename)
+        vim.cmd(line_number)
+    else
+        print('Usage: ReopenInLastTabAtLine <line_number> "<filename>"')
+    end
+end, { nargs = 1, complete = "file", desc = "Used by lazygit to open hunks, tested on Windows" })
+
+usercmd("ReopenInLastTab", function(opts)
+    local filename = opts.args:match('%"([^%"]+)%"')
+    if filename then
+        vim.cmd("bdelete")
+        vim.cmd("edit " .. filename)
+    else
+        print('Usage: ReopenInLastTab "<filename>"')
+    end
+end, { nargs = 1, complete = "file", desc = "Used by lazygit to open hunks, tested on Windows" })
--- nvim/lua/radlinskii/plugins/lazygit.lua
+++ nvim/lua/radlinskii/plugins/lazygit.lua
@@ -14,6 +14,10 @@ return {
     -- setting the keybinding for LazyGit with 'keys' is recommended in
     -- order to load the plugin when the command is run for the first time
     keys = {
-        { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+        { "<leader>g", "<cmd>LazyGit<cr>", desc = "LazyGit" },
     },
+    init = function()
+        vim.g.lazygit_floating_window_scaling_factor = 0.9 -- 1
+        vim.g.lazygit_floating_window_use_plenary = 1
+    end,
 }
