58ebdca0b0a224489ef5fab740af6585a88d87f4
radlinskii <radlinskiignacy@gmail.com>
2024-07-04 17:42:56 +0200

    try adding mini.icons

---
+++ nvim/lua/radlinskii/plugins/mini-icons.lua
@@ -0,0 +1,18 @@
+return {
+    -- "echasnovski/mini.icons",
+    -- opts = {
+    --     -- Customize per category. See `:h MiniIcons.config` for details.
+    -- },
+    -- lazy = true,
+    -- specs = {
+    --     { "nvim-tree/nvim-web-devicons", enabled = false, optional = true },
+    -- },
+    -- init = function()
+    --     package.preload["nvim-web-devicons"] = function()
+    --         -- needed since it will be false when loading and mini will fail
+    --         package.loaded["nvim-web-devicons"] = {}
+    --         require("mini.icons").mock_nvim_web_devicons()
+    --         return package.loaded["nvim-web-devicons"]
+    --     end
+    -- end,
+}
