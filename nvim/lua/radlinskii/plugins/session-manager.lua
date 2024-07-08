0ebe1a3364c28b7b8e66f7bff1edd8c9288b16fc
radlinskii <radlinskiignacy@gmail.com>
2024-07-08 15:35:16 +0200

    remove unused session plugins

--- nvim/lua/radlinskii/plugins/session-manager.lua
+++ nvim/lua/radlinskii/plugins/session-manager.lua
@@ -1,49 +1,6 @@
 ---@type LazyPluginSpec[]
 return {
     {
-        "Shatur/neovim-session-manager",
-        lazy = false,
-        enabled = false,
-        config = function()
-            local config = require("session_manager.config")
-
-            require("session_manager").setup({
-                autoload_mode = config.AutoloadMode.CurrentDir,
-                autosave_ignore_dirs = { "/", "~", "/tmp/", vim.fn.expand("~"), vim.fn.expand("~" .. "/Projects/") },
-            })
-        end,
-    },
-
-    {
-        "stevearc/resession.nvim",
-        opts = {},
-        lazy = false,
-        enabled = false,
-        config = function()
-            local resession = require("resession")
-
-            resession.setup()
-
-            vim.api.nvim_create_autocmd("VimEnter", {
-                callback = function()
-                    -- Only load the session if nvim was started with no args
-                    if vim.fn.argc(-1) == 0 then
-                        -- Save these to a different directory, so our manual sessions don't get polluted
-                        resession.load(vim.fn.getcwd(), { dir = "dirsession", silence_errors = true })
-                    end
-                end,
-                nested = true,
-            })
-
-            vim.api.nvim_create_autocmd("VimLeavePre", {
-                callback = function()
-                    resession.save(vim.fn.getcwd(), { dir = "dirsession", notify = false })
-                end,
-            })
-        end,
-    },
-
-    {
         "rmagatti/auto-session",
         lazy = false,
         enabled = true,
