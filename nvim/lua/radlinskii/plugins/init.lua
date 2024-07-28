ce152e1693c80700c6ec45d1dc9c3f39530127e5
radlinskii <radlinskiignacy@gmail.com>
2024-07-28 22:22:58 +0200

    add snipe plugin

--- nvim/lua/radlinskii/plugins/init.lua
+++ nvim/lua/radlinskii/plugins/init.lua
@@ -1,2 +1,24 @@
 ---@type LazyPluginSpec[]
-return {}
+return {
+    {
+        "leath-dub/snipe.nvim",
+        keys = {
+            {
+                "<leader>rb",
+                function()
+                    require("snipe").create_buffer_menu_toggler()()
+                end,
+                desc = "List buffers in Snipe window",
+            },
+        },
+        opts = {
+            hints = {
+                dictionary = "tsragpfwqdcxzhkmjo",
+            },
+            navigate = {
+                next_page = "U",
+                prev_page = "E",
+            },
+        },
+    },
+}
--- nvim/lua/radlinskii/plugins/noice-nvim.lua
+++ nvim/lua/radlinskii/plugins/noice-nvim.lua
@@ -23,6 +23,8 @@ return {
                 ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
                 ["vim.lsp.util.stylize_markdown"] = true,
                 ["cmp.entry.get_documentation"] = true,
+                ["config.lsp.hover.enabled"] = true,
+                ["config.lsp.signature.enabled"] = true,
             },
         },
         presets = {
@@ -30,7 +32,7 @@ return {
             command_palette = false, -- position the cmdline and popupmenu together
             long_message_to_split = true, -- long messages will be sent to a split
             inc_rename = false, -- enables an input dialog for inc-rename.nvim
-            lsp_doc_border = false, -- add a border to hover docs and signature help
+            lsp_doc_border = true, -- add a border to hover docs and signature help
         },
 
         -- default messages to mini
