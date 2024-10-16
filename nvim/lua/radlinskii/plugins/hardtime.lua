7205a5c30d98cde3c54d3981fef62e4bd87468b3
radlinskii <radlinskiignacy@gmail.com>
2024-10-16 16:58:22 +0200

    add hardtime plugin

---
+++ nvim/lua/radlinskii/plugins/hardtime.lua
@@ -0,0 +1,22 @@
+---@type LazyPluginSpec
+return {
+    "m4xshen/hardtime.nvim",
+    dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+    event = { "BufReadPre", "BufNewFile" },
+    opts = {
+        disable_mouse = false,
+        restricted_keys = {
+            ["<Up>"] = { "n", "x" },
+            ["<Down>"] = { "n", "x" },
+            ["<Left>"] = { "n", "x" },
+            ["<Right>"] = { "n", "x" },
+        },
+        disabled_keys = {
+            ["<Up>"] = {},
+            ["<Down>"] = {},
+            ["<Left>"] = {},
+            ["<Right>"] = {},
+        },
+        disabled_filetypes = {},
+    },
+}
