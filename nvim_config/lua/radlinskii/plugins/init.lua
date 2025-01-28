5632e856642f65bfa28a69512035906fb87e1beb
radlinskii <radlinskiignacy@gmail.com>
2025-01-28 10:57:19 +0100

    add smear-cursor.nvim

--- nvim_config/lua/radlinskii/plugins/init.lua
+++ nvim_config/lua/radlinskii/plugins/init.lua
@@ -1,2 +1,14 @@
 ---@type LazyPluginSpec[]
-return {}
+return {
+    {
+        "sphamba/smear-cursor.nvim",
+        event = { "BufReadPre", "BufNewFile" },
+        opts = {
+            stiffness = 0.8, -- 0.6 - [0, 1]
+            trailing_stiffness = 0.5, -- 0.3 - [0, 1]
+            distance_stop_animating = 0.5, -- 0.1 - > 0
+            hide_target_hack = false,
+            legacy_computing_symbols_support = true,
+        },
+    },
+}
