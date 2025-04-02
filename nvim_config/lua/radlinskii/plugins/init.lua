d26e73568b0d42638c1739979bc5b45196ff7d9b
radlinskii <radlinskiignacy@gmail.com>
2025-04-02 21:42:40 +0200

    remove neoscroll

--- nvim_config/lua/radlinskii/plugins/init.lua
+++ nvim_config/lua/radlinskii/plugins/init.lua
@@ -1,11 +1,2 @@
 ---@type LazyPluginSpec[]
-return {
-    {
-        enabled = not require("radlinskii.utils.system").is_windows(),
-        "karb94/neoscroll.nvim",
-        event = { "BufReadPre", "BufNewFile" },
-        opts = {
-            duration_multiplier = 0.5,
-        },
-    },
-}
+return {}
