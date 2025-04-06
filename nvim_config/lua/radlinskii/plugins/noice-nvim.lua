81293cb4d1738755d73db8b8fa1973174e409c61
radlinskii <radlinskiignacy@gmail.com>
2025-04-06 18:00:32 +0200

    cleanup noice config

--- nvim_config/lua/radlinskii/plugins/noice-nvim.lua
+++ nvim_config/lua/radlinskii/plugins/noice-nvim.lua
@@ -2,6 +2,35 @@
 return {
     "folke/noice.nvim",
     event = { "VeryLazy" },
+    dependencies = {
+        "MunifTanjim/nui.nvim",
+        {
+            "rcarriga/nvim-notify",
+            opts = {
+                timeout = 2500,
+                max_height = function()
+                    return math.floor(vim.o.lines * 0.75)
+                end,
+                max_width = function()
+                    return math.floor(vim.o.columns * 0.40)
+                end,
+                on_open = function(win)
+                    vim.api.nvim_win_set_config(win, { focusable = false })
+                end,
+                render = "wrapped-compact",
+                stages = "slide",
+            },
+        },
+    },
+    keys = {
+        {
+            "<leader>nn",
+            function()
+                require("noice").cmd("dismiss")
+            end,
+            desc = "Dismiss all notifications",
+        },
+    },
     opts = {
         routes = {
             {
@@ -25,53 +54,17 @@ return {
             },
         },
         lsp = {
-            -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
             override = {
                 ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
                 ["vim.lsp.util.stylize_markdown"] = true,
-                ["cmp.entry.get_documentation"] = true,
-                ["config.lsp.hover.enabled"] = true,
-                ["config.lsp.signature.enabled"] = true,
             },
         },
         presets = {
-            bottom_search = false, -- use a classic bottom cmdline for search
-            command_palette = false, -- position the cmdline and popupmenu together
-            long_message_to_split = true, -- long messages will be sent to a split
-            inc_rename = false, -- enables an input dialog for inc-rename.nvim
-            lsp_doc_border = true, -- add a border to hover docs and signature help
-        },
-
-        -- default messages to mini
-        -- messages = { view = "mini", view_warn = "mini" },
-    },
-    dependencies = {
-        "MunifTanjim/nui.nvim",
-        {
-            "rcarriga/nvim-notify",
-            opts = {
-                timeout = 2500,
-                max_height = function()
-                    return math.floor(vim.o.lines * 0.75)
-                end,
-                max_width = function()
-                    return math.floor(vim.o.columns * 0.40)
-                end,
-                on_open = function(win)
-                    vim.api.nvim_win_set_config(win, { focusable = false })
-                end,
-                render = "wrapped-compact",
-                stages = "slide", -- "static",
-            },
-        },
-    },
-    keys = {
-        {
-            "<leader>nn",
-            function()
-                require("noice").cmd("dismiss")
-            end,
-            desc = "Dismiss all notifications",
+            bottom_search = true,
+            command_palette = false,
+            long_message_to_split = true,
+            inc_rename = false,
+            lsp_doc_border = true,
         },
     },
 }
