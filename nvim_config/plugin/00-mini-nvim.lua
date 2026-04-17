54d51b791c936cbb458bfb78d0ba9db86d7ff8cd
radlinskii <radlinskiignacy@gmail.com>
2026-04-17 18:45:56 +0200

    replace another packages with mini

--- nvim_config/plugin/00-mini-nvim.lua
+++ nvim_config/plugin/00-mini-nvim.lua
@@ -40,3 +40,21 @@ require("mini.pairs").setup()
 require("mini.trailspace").setup()
 
 require("mini.cursorword").setup()
+
+require("mini.comment").setup()
+
+require("mini.surround").setup({
+    mappings = {
+        add = "ys",
+        delete = "ds",
+        find = "",
+        find_left = "",
+        highlight = "",
+        replace = "cs",
+
+        -- Add this only if you don't want to use extended mappings
+        suffix_last = "",
+        suffix_next = "",
+    },
+    search_method = "cover_or_next",
+})
--- nvim_config/plugin/matchup.lua
+++
@@ -1,8 +0,0 @@
-vim.api.nvim_create_autocmd({ "BufReadPre", "BufNewFile", "BufEnter" }, {
-    once = true,
-    callback = function()
-        vim.pack.add({ "https://github.com/andymass/vim-matchup" })
-
-        vim.g.matchup_matchparen_offscreen = { method = "popup" }
-    end,
-})
